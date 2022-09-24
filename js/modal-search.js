812d8308b84a3344a913bcc419df1e52b43ea9cb
Timur_Chemazokov <79016313+ChemTi@users.noreply.github.com>
2022-09-24 17:50:17 +0300

    add ease validation to modal-search.js

--- js/modal-search.js
+++ js/modal-search.js
@@ -1,22 +1,35 @@
 const modal = document.querySelector(".modal-search");
 const buttonSearch = document.querySelector(".search__button-search");
+const buttonFind = modal.querySelector(".modal-search__button-find");
+const dateIn = modal.querySelector("[name=date-in]");
+const dateOut = modal.querySelector("[name=date-out]");
 
 // open/close modal by button
-  buttonSearch.addEventListener("click", (e) => {
-    e.preventDefault();
-    modal.classList.toggle("display-none");
-    e.stopPropagation();
-  });
+buttonSearch.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.classList.toggle("display-none");
+  e.stopPropagation();
+  dateIn.focus();
+});
 
-  // close modal by click outside
-  document.addEventListener('click', (e) => {
-    if (!e.target.closest(".modal-search"))
+// close modal by click outside
+document.addEventListener('click', (e) => {
+  if (!e.target.closest(".modal-search"))
     modal.classList.add("display-none")
-  });
+});
 
 // close modal by Escape
-  document.addEventListener('keydown', (e) => {
-    if (e.key === "Escape" && !modal.classList.contains("display-none")) {
-      modal.classList.add("display-none");
-    }
-  })
+window.addEventListener('keydown', (e) => {
+  if (e.key === "Escape" && !modal.classList.contains("display-none")) {
+    e.preventDefault();
+    modal.classList.add("display-none");
+  }
+})
+
+// ease validation
+buttonFind.addEventListener("click", (e) => {
+  if (!dateIn.value || !dateOut.value) {
+    e.preventDefault();
+    alert("Вы не ввели дату ЗАЕЗДА или дату ВЫЕЗДА");
+  } else localStorage.setItem(dateIn, dateIn.value);
+});
